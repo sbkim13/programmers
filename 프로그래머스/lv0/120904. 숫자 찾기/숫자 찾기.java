c979c63cad04b3052fd4e2afb9f272dde7508299
김샛별(Kim SaetByul) <ahoho0544@gmail.com>
2023-04-11 13:35:12 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -3,19 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int num, int k) {
         String strNum = Integer.toString(num);
-        String[] arr = strNum.split("");
-        int res = -1;
-        
-        for(int i = 0; i < arr.length; i++) {
-        	int tmp = Integer.parseInt(arr[i]);
-        	if(tmp == k) {
-        		res = i + 1;
-        		break;
-        	}
-        }
-        
-        System.out.print("연동test");
-        
-        return res;
+        int idx = strNum.indexOf(Integer.toString(k));
+       
+        return idx > -1 ? idx + 1 : idx;
     }
 }

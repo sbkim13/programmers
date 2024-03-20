6c652dc5cb5b95e317e0b60d4ae2607d4cd9279a
김샛별(Kim SaetByul) <ahoho0544@gmail.com>
2024-03-20 09:54:54 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 94.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if(is_prefix.length() > my_string.length()) {
+            return 0;
+        }
+        
+        int answer = my_string.substring(0, is_prefix.length()).equals(is_prefix) ? 1 : 0;
+        return answer;
+    }
+}
